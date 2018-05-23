72e8609a0d5828eb64e03ca380ce6f46e8b27016
leonidlezner <kontakt@lezner.de>
2018-05-23 21:05:47 +0200

    Initial commit

---
+++ tests/Feature/LoginRegisterTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginRegisterTest extends TestCase
+{    
+    use DatabaseTransactions;
+
+    public function test_guest_sees_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_guest_cannot_login()
+    {
+        $params = [
+            'email' => 'fake@mail.boo',
+            'password' => 'ABCDEFG123'
+        ];
+
+        $response = $this->post('/login', $params);
+
+        $response->assertSessionHasErrors();
+
+        $this->assertGuest();
+    }
+
+    public function test_guest_can_register()
+    {
+        $user_data = [
+            'name' => 'Test Testman',
+            'email' => 'test@testing.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret'
+        ];
+
+        $response = $this->post('/register', $user_data);
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('users', [
+            'email' => $user_data['email']
+        ]);
+
+        $this->assertAuthenticated();
+
+        $user = \App\User::where('email', $user_data['email'])->first();
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_guest_cannot_register_if_registered()
+    {
+        $user = factory(\App\User::class)->create();
+
+        $user_data = [
+            'name' => 'dont care',
+            'email' => $user->email,
+            'password' => 'secret',
+            'password_confirmation' => 'secret'
+        ];
+
+        $response = $this->post('/register', $user_data);
+
+        $response->assertStatus(302);
+
+        $response->assertSessionHasErrors();
+        
+        $this->assertGuest();
+    }
+
+    public function test_user_can_login()
+    {
+        $user = factory(\App\User::class)->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_cannot_login_wrong_pass()
+    {
+        $user = factory(\App\User::class)->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrongpassword'
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertGuest();
+    }
+    
+    public function test_user_can_logout()
+    {
+        $user = factory(\App\User::class)->create();
+
+        $response = $this->actingAs($user)->post('/logout');
+
+        $response->assertStatus(302);
+
+        $this->assertGuest();
+    }
+}
